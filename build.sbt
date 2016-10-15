7d46653f527a4736be1809d0e4e87e0c376c8395
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-10-15 15:55:08 +0200

    update import and angulillos, do full import, clean a bit

--- build.sbt
+++ build.sbt
@@ -7,15 +7,11 @@ description   := "data project"
 bucketSuffix  := "era7.com"
 
 libraryDependencies ++= Seq(
-  "bio4j"                   %%  "import"            % "0.0.0-85-gd4b3c8c",
-  "bio4j"                   %   "angulillos-titan"  % "0.4.8",
+  "bio4j"                   %%  "import"            % "0.0.0-96-gbb3c223",
+  "bio4j"                   %   "angulillos-titan"  % "0.4.9",
   "com.thinkaurelius.titan" %   "titan-berkeleyje"  % "1.0.0",
   "ohnosequences"           %%  "fastarious"        % "0.6.0"
 ) ++ testDependencies
 
 
 val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.0.0" % Test)
-
-dependencyOverrides ++= Set(
-  "bio4j" % "angulillos" % "0.9.0-20-g2b2e685"
-)
--- src/test/scala/Data.scala
+++ src/test/scala/Data.scala
@@ -12,13 +12,22 @@ import org.scalatest.FunSuite
 
 class SwissProtTests extends FunSuite {
 
+  type G = UniProtGraph[TitanVertex,TitanEdge]
+  type G0 = com.bio4j.angulillos.TypedGraph[G,TitanVertex,TitanEdge]
+
+  lazy val conf =
+    ReadOnlyTitan(new java.io.File("db"))
+
+  lazy val uniProtGraph =
+    new UniProtGraph(conf.untypedGraph)
+
   test("find protein by gene name") {
 
-    // val z: Option[String] =
-    //   uniProtGraph.geneName.name.index.find("APP").asScala map { _.get[String, UniProtGraph[TitanVertex,TitanEdge]#GeneNameType#Name](uniProtGraph.geneName.name) }
-    //
-    // println { z }
-    //
+    val z: Option[String] =
+      uniProtGraph.geneName.name.index.find("APP").asScala map { _.get[String, G#GeneNameType#Name](uniProtGraph.geneName.name) }
+
+    assert { z == Some("APP") }
+
     // val allNames = uniProtGraph.geneName.vertices().iterator().asScala.zipWithIndex foreach {
     //   case (gn,index) =>
     //     if(index % 1000 == 0) {
@@ -29,5 +38,18 @@ class SwissProtTests extends FunSuite {
     // }
   }
 
-  ignore("Import annotations") {}
+  test("acute phase proteins") {
+
+    val proteinAccessions = uniProtGraph.keyword.id.index.find("KW-0024").asScala.map { kw =>
+      kw.inV[G#Protein, G0#VertexType[G#Protein], G#Keywords, G0#EdgeType[G#Protein,G#Keywords,G#Keyword]](uniProtGraph.keywords).iterator().asScala.toList.map { protein =>
+
+        protein.getOpt[String](uniProtGraph.protein.accession).asScala
+      }
+      .flatten
+    }
+
+    proteinAccessions.foreach { l => println(l.size) }
+
+    println{ proteinAccessions }
+  }
 }
